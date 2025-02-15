7648d757f9bd35a27c1a4ce46f8923c3c4027a22
Abhishek Verma <abhishekverma@Abhisheks-MacBook-Pro-4.local>
2025-02-15 14:37:28 +0530

    removed dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import { ThemeProvider } from "@/components/theme-provider"
 import SessionProviderWrapper from "@/components/SessionProviderWrapper";
 import { QueryProvider } from '@/components/QueryWrapper';
 import SessionRedirect from '@/components/SessionRedirect';
