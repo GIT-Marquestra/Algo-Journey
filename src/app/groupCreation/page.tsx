6e117cb8ea8b0448e57cf07971246e042070e4b8
anish877 <62011810+anish877@users.noreply.github.com>
2025-02-17 16:40:51 +0530

    delete group

--- src/app/groupCreation/page.tsx
+++ src/app/groupCreation/page.tsx
@@ -4,43 +4,40 @@ import AdminGroupCreator from '@/components/Group'
 import GroupMemberAdder from '@/components/GroupMemberAdder'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-
 interface Group {
   id: string,
   name: string
-} 
-
+}
 function Page() {
-  const [show, setShow] = useState('')
+  const [show, setShow] = useState(false)
+  const [showAdminSpecific, setShowAdminSpecific] = useState(false)
   const [group, setGroup] = useState<Group>()
   useEffect(() => {
     const func = async () => {
       const res1 = await axios.post('api/checkIfAdmin')
       if(res1.data.isAdmin){
-        setShow('admin')
+        setShowAdminSpecific(true)
       }
       const res2 = await axios.post('api/isCoordinator')
       if(res2.data.isCoordinator){
-        setShow('coord')
+        setShow(true)
       }
       const response = await axios.post('api/getGroup')
       setGroup(response.data.group)
       console.log(response.data)
     }
-
     func()
   }, [])
   return (
     <div>
       <div>
-        <GroupManagement/>
+        <GroupManagement isAdmin={showAdminSpecific}/>
       </div>
       <div className='mt-5'>
-        {show === 'coord' && group && <GroupMemberAdder groupId={group.id} groupName={group.name}/>} 
-        {show === 'admin' && <AdminGroupCreator/>}        
+        {show && group && <GroupMemberAdder groupId={group.id} groupName={group.name}/>}
+        {showAdminSpecific && <AdminGroupCreator/>}
       </div>
     </div>
   )
 }
-
 export default Page
