0cc7e9d2b8a1328e80329e3602bb2e4e142fb4f8
anish877 <62011810+anish877@users.noreply.github.com>
2025-02-18 09:38:11 +0530

    protect admin dashboard page

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,7 +1,22 @@
+'use client';
 import AllQuestions from '@/components/AllQuestions'
-import React from 'react'
+import axios from 'axios';
+import { useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 function Page() {
+  const [isAdmin, setIsAdmin] = useState(false)
+  useEffect(() => {
+    const func = async () => {
+      const res1 = await axios.post('../api/checkIfAdmin')
+      if(res1.data.isAdmin)
+        setIsAdmin(true)
+    }
+    func()
+    if(!isAdmin)
+      redirect("/user/dashboard")
+  }, [])
   return (
     <div className='flex items-center justify-center pt-16 w-full h-screen'>
       <div className='w-full h-full overflow-auto'>
