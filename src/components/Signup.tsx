ef6c1c08438bed9ee3c07407229563ec5f98a4a9
GIT-Marquestra <vabhi.verma2678@gmail.com>
2025-03-10 15:10:57 +0530

    removed extra allowance of teacher email

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -40,7 +40,7 @@ import { fetchLatestSubmissionsCodeForces, fetchLatestSubmissionsLeetCode } from
 const signupSchema = z.object({
   username: z.string().min(3, "Username must be at least 3 characters"),
   email: z.string().email("Invalid email").refine(
-    (email) => email.endsWith("@nst.rishihood.edu.in") || email.endsWith('@newtonschool.co'),
+    (email) => email.endsWith("@nst.rishihood.edu.in"),
     "Must use college email"
   ),
   password: z.string()
