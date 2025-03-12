22c5391fb80f1bffff0d21edbc8ba056a2afbd36
GIT-Marquestra <vabhi.verma2678@gmail.com>
2025-03-12 20:23:09 +0530

    improved signin

--- src/components/QuestionSolving.tsx
+++ src/components/QuestionSolving.tsx
@@ -63,9 +63,9 @@ interface QuestionTag {
 }
 
 const AVAILABLE_TAGS = [
-  "PrefixSum", "TwoPointers", "1D Arrays", "Graph", "2D Arrays", 
-  "Time complexity", "Basic Maths", "Space complexity", "BinarySearch", 
-  "DP", "Sorting", "Linear search", "Exponentiation", "Recursion", "String"
+  "PrefixSum", "TwoPointers", "1DArrays", "Graph", "2DArrays", 
+  "TimeComplexity", "BasicMaths", "SpaceComplexity", "BinarySearch", 
+  "DP", "Sorting", "LinearSearch", "Exponentiation", "Recursion", "String"
 ];
 
 const DIFFICULTIES = [
