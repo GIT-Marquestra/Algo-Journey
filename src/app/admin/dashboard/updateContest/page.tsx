066a901d8b39d7687b101368ab1125e52e4a49a9
GIT-Marquestra <vabhi.verma2678@gmail.com>
2025-03-21 20:21:27 +0530

    done till here

--- src/app/admin/dashboard/updateContest/page.tsx
+++ src/app/admin/dashboard/updateContest/page.tsx
@@ -1,21 +1,26 @@
 'use client'
 import UpdateContestCard, { Question } from '@/components/UpdateContest'
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast';
 
-async function Page() {
+function Page() {
   const [questions, setQuestions] = useState<Question[]>([])
-  try {
-    const response = await axios.post<{ questions: Question[] }>("/api/getQuestions");
-    if (response.status === 200) {
-      setQuestions(response.data.questions);
-    } else {
+  const fetch = async () => {
+    try {
+      const response = await axios.post<{ questions: Question[] }>("/api/getQuestions");
+      if (response.status === 200) {
+        setQuestions(response.data.questions);
+      } else {
+        toast.error("Failed to fetch questions");
+      }
+    } catch (error) {
+      console.error("Error fetching questions:", error);
       toast.error("Failed to fetch questions");
-    }
-  } catch (error) {
-    console.error("Error fetching questions:", error);
-    toast.error("Failed to fetch questions");
+  }
+  useEffect(() => {
+    fetch()
+  }, [])
   return (
     <div>
       <UpdateContestCard dbQuestions={questions}/>
